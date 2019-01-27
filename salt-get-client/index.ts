1d6f902eecdbcdf0badf0fb28a907cab915550ae
Mark Mayer <mdm373@gmail.com>
2019-01-26 22:54:56 -0500

    blink demo

--- salt-get-client/index.ts
+++ salt-get-client/index.ts
@@ -1,2 +1,21 @@
-const a: string = process.env.NODE_ENV || 'undefined';
-console.log(`node env is '${a}'`);
+import {Gpio} from 'onoff';
+import {timer, from, of} from 'rxjs';
+import { switchMap, map, catchError } from 'rxjs/operators';
+
+
+
+const led = new Gpio(4, 'out');
+const readLed = () => new Promise<number>((accept, reject) => led.read((error, value) => error ? reject(error) : accept(value)));
+const writeLed = (value: number) => new Promise((accept, reject) => led.write(value, (error, value) => error ? reject(error) : accept(value)));
+
+timer(1000, 1000).pipe(
+  switchMap(() => from(readLed())),
+  switchMap((value) => {
+    const newValue = value === 0 ? 1 : 0;
+    return from(writeLed(newValue));
+  }),
+  map((newValue) => newValue === 0 ? 'off' : 'on'),
+  catchError((error) => of('!!ERROR!!' + JSON.stringify(error)))
+).subscribe((updatedValue)=>{
+  console.log(`blinking: ${updatedValue}`);
+});
