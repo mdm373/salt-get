d0d4376656b26637dbbc643921fbcb7cdb555ca7
Mark Mayer <mdm373@gmail.com>
2019-01-27 10:46:04 -0500

    updates for echo reading / tslint

--- salt-get-client/blink.ts
+++ salt-get-client/blink.ts
@@ -1,12 +1,12 @@
-import {Gpio} from 'onoff';
-import {timer, from, of} from 'rxjs';
-import { switchMap, map, catchError } from 'rxjs/operators';
+import {Gpio} from "onoff";
+import {from, of, timer} from "rxjs";
+import { catchError, map, switchMap } from "rxjs/operators";
 
-
-
-const led = new Gpio(4, 'out');
-const readLed = () => new Promise<number>((accept, reject) => led.read((error, value) => error ? reject(error) : accept(value)));
-const writeLed = (value: number) => new Promise((accept, reject) => led.write(value, (error, value) => error ? reject(error) : accept(value)));
+const led = new Gpio(4, "out");
+const readLed = () => new Promise<number>((accept, reject) =>
+  led.read((error, value) => error ? reject(error) : accept(value)));
+const writeLed = (value: number) =>
+  new Promise((accept, reject) => led.write(value, (error, writeVal) => error ? reject(error) : accept(writeVal)));
 
 timer(1000, 1000).pipe(
   switchMap(() => from(readLed())),
@@ -14,8 +14,8 @@ timer(1000, 1000).pipe(
     const newValue = value === 0 ? 1 : 0;
     return from(writeLed(newValue));
   }),
-  map((newValue) => newValue === 0 ? 'off' : 'on'),
-  catchError((error) => of('!!ERROR!!' + JSON.stringify(error)))
-).subscribe((updatedValue)=>{
+  map((newValue) => newValue === 0 ? "off" : "on"),
+  catchError((error) => of("!!ERROR!!" + JSON.stringify(error))),
+).subscribe((updatedValue) => {
   console.log(`blinking: ${updatedValue}`);
 });
--- salt-get-client/echo.ts
+++ salt-get-client/echo.ts
@@ -1,47 +1,47 @@
-import {Subject, timer, from} from 'rxjs';
-import {Gpio, tickDiff} from 'pigpio';
-import { switchMap } from 'rxjs/operators';
+import {Gpio, tickDiff} from "pigpio";
+import {from, Subject, timer} from "rxjs";
+import { switchMap } from "rxjs/operators";
 
-enum IODirection {in=0, out=1};
-enum IOLevel {low=0, high=1};
+enum IODirection {in= 0, out= 1}
+enum IOLevel {low= 0, high= 1}
 interface WatchData {
   readonly level: number;
   readonly tick: number;
   readonly delta?: number;
 }
-const sleep = (time: number) => new Promise<void>((accept) => setInterval(accept, time));
 const getIo = (num: number, dir: IODirection, edge: boolean) => {
   const io = new Gpio(num, {mode: dir, alert: edge});
   const read = () => new Promise<number>((accept) => accept(io.digitalRead()));
   const write = (value: number) => new Promise<void>((accept) => {
-    io.digitalWrite(value)
+    io.digitalWrite(value);
     accept();
   });
   const subj = new Subject<WatchData>();
   let lastTick: number;
-  io.on('alert', (level:number, tick:number) => {
-    const delta = lastTick === undefined ? undefined: tickDiff(lastTick, tick)
-    subj.next({level, tick,delta});
+  io.on("alert", (level: number, tick: number) => {
+    const delta = lastTick === undefined ? undefined : tickDiff(lastTick, tick);
+    lastTick = tick;
+    subj.next({level, tick, delta});
   });
   const trigger = async (level: IOLevel, duration: number) => {
     await write(level === IOLevel.high ? IOLevel.low : IOLevel.high);
-    io.trigger(duration, level)
+    io.trigger(duration, level);
   };
   const watch = subj.asObservable();
   return {read, write, watch, trigger};
 };
 
-const microsecondsPerCm = 1e6/34321;
+const microsecondsPerCm = 1e6 / 34321;
 (async () => {
   const trigger = getIo(5, IODirection.out, false);
   const echo = getIo(6, IODirection.in, true);
   echo.watch.subscribe((data) => {
-    console.log('echo value', data);
-    if(data.delta){
-      console.log(`distance is '${data.delta/2/microsecondsPerCm}' cm`);
+    console.log("echo value", data);
+    if (data.delta && data.level === IOLevel.low) {
+      console.log(`distance is '${data.delta / 2 / microsecondsPerCm}' cm`);
     }
   });
   timer(5000, 5000).pipe(
-    switchMap( () => from(trigger.trigger(IOLevel.high, 10)))
+    switchMap( () => from(trigger.trigger(IOLevel.high, 10))),
   ).subscribe();
 })();
