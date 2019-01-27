8b917a292a2afc3eb0ff19e65070ce3b5a802493
Mark Mayer <mdm373@gmail.com>
2019-01-27 10:30:23 -0500

    trying with pigpio

--- salt-get-client/echo.ts
+++ salt-get-client/echo.ts
@@ -1,35 +1,47 @@
-import {Gpio, Direction} from 'onoff';
-import {timer, from, of, BehaviorSubject} from 'rxjs';
-import { switchMap, map, catchError } from 'rxjs/operators';
+import {Subject, timer, from} from 'rxjs';
+import {Gpio, tickDiff} from 'pigpio';
+import { switchMap } from 'rxjs/operators';
 
-
-// const sleep = (time: number) => new Promise<void>((accept) => setInterval(accept, time));
-const getIo = (num: number, dir: Direction, edge: boolean) => {
-  const io = new Gpio(num, dir, edge ? 'both' : 'none');
-  const read = () => new Promise<number>((accept, reject) => io.read((error, value) => error ? reject(error) : accept(value)));
-  const write = (value: number) => new Promise((accept, reject) => io.write(value, (error, value) => error ? reject(error) : accept(value)));
-  const subj = new BehaviorSubject<number>(0);
-  io.watch((error, value) => {
-    error ? subj.error(error) : subj.next(value);
+enum IODirection {in=0, out=1};
+enum IOLevel {low=0, high=1};
+interface WatchData {
+  readonly level: number;
+  readonly tick: number;
+  readonly delta?: number;
+}
+const sleep = (time: number) => new Promise<void>((accept) => setInterval(accept, time));
+const getIo = (num: number, dir: IODirection, edge: boolean) => {
+  const io = new Gpio(num, {mode: dir, alert: edge});
+  const read = () => new Promise<number>((accept) => accept(io.digitalRead()));
+  const write = (value: number) => new Promise<void>((accept) => {
+    io.digitalWrite(value)
+    accept();
   });
+  const subj = new Subject<WatchData>();
+  let lastTick: number;
+  io.on('alert', (level:number, tick:number) => {
+    const delta = lastTick === undefined ? undefined: tickDiff(lastTick, tick)
+    subj.next({level, tick,delta});
+  });
+  const trigger = async (level: IOLevel, duration: number) => {
+    await write(level === IOLevel.high ? IOLevel.low : IOLevel.high);
+    io.trigger(duration, level)
+  };
   const watch = subj.asObservable();
-  return {read, write, watch};
+  return {read, write, watch, trigger};
 };
 
-const trigger = getIo(5, 'out', false);
-const echo = getIo(6, 'in', true);
-
-let start: number = 0;
-echo.watch.subscribe((value) => {
-  if(value){
-    start = (new Date()).getTime();
-  } else if(start > 0) {
-    const duration = (new Date()).getTime() - start;
-    console.log(duration);
-    start = 0;
-  }
-});
+const microsecondsPerCm = 1e6/34321;
 (async () => {
-  await trigger.write(1);
-  await trigger.write(0);
+  const trigger = getIo(5, IODirection.out, false);
+  const echo = getIo(6, IODirection.in, true);
+  echo.watch.subscribe((data) => {
+    console.log('echo value', data);
+    if(data.delta){
+      console.log(`distance is '${data.delta/2/microsecondsPerCm}' cm`);
+    }
+  });
+  timer(5000, 5000).pipe(
+    switchMap( () => from(trigger.trigger(IOLevel.high, 10)))
+  ).subscribe();
 })();
