e43e981be76bfd67c279cf3aa65236a1097f8c05
Mark Mayer <mdm373@gmail.com>
2022-01-03 16:46:31 -0500

    refactor

--- salt-get-zero/src/__main__.py
+++ salt-get-zero/src/__main__.py
@@ -2,10 +2,12 @@ from hcsr04_range_module import make_module
 
 TRIGGER = 23
 ECHO = 24
-
+COUNT = 5;
 
 if __name__ == "__main__":
     ping = make_module(TRIGGER, ECHO)
-    print(ping())
-    print(ping())
-    print(ping())
+    total = 0
+    for x in range(COUNT):
+        total += ping()
+    average = round(total / COUNT, 2)
+    print(average)
